528a8ac5cba60f19375f82eb7f86896bd89545fd
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-07-27 15:24:09 +0200

    add create update & destroy + private methods to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+  before_action :find_review, only: [ :update, :destroy ]
+
+  def create
+    @game = Game.find(params[:game_id])
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
