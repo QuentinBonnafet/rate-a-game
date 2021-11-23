66fb8306a066adf4ad3480d3358ebcd90c72f095
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-11-23 15:57:23 +0100

    game new create & strong params in comment for later

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,22 @@
 class GamesController < ApplicationController
+
+  def index
+    
+  end
+
+  # def new
+  #   @game = Game.new
+  # end
+
+  # def create
+  #   @game = Game.new(game_params)
+  #   @game.save
+  # end
+
+  # private
+
+  # def game_params
+  #   params.require(:game).permit(:name, :studio, :plateform, :genre, :year)
+  # end
+
 end
