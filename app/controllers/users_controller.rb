6402e1721740bb01fe12f1399b117bfa10aeb4b3
Quentin <q.bonnafet@studio-hb.com>
2022-03-02 16:46:56 +0100

    add method create & strong params in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password)
+  end
 end
